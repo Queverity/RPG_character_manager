0bb52ee302835d3398432c49153c2a53a3f90507
Queverity <pryor.baird@ucas-edu.net>
2026-04-02 04:35:36 +0000

    finished pseudocode

--- new_code/data_visulisation.py
+++ new_code/data_visulisation.py
@@ -2,9 +2,16 @@
 
 # use matplotlib for this
 
-import matplotlib.pyplot as plt
-import numpy as np
 
-fig, ax = plt.subplots()             # Create a figure containing a single Axes.
-ax.plot([1, 2, 3, 4], [1, 4, 2, 3])  # Plot some data on the Axes.
-plt.show()                           # Show the figure.
+
+# class DataVisulisation():
+    # def __init__:
+        # for a matplotlib bar chart, we'll need a list of values (attributes), our categories (attribute names), and maybe a few other thins
+        # we'll need to be able to generate charts with the attributes of two character side by side (maybe attach a color to each character)
+
+    # def single_plt():
+        # take attribute information from a single character, generate a bar graph to display it, and return that bar graph
+
+    # def comparison_plt():
+        # take attribute information from two characters, generate a bar graph with each attribute side by side, assign a color to each character so it is readable, and return that bar graph
+
---
+++ new_code/statistacal_analysis.py
@@ -0,0 +1,22 @@
+# CB 1st Character Statistacal Analyasis
+
+# from character_class import *
+
+# define class StatisticalAnalyzer:
+    # def __init__():
+        # get list of character objects, load into panda dataframes (figure out what that means exactly)
+        # use read_csv to load into dataframes
+
+    # define method filter_data():
+        # give user multiple options on how to filter data that is loaded into the dataframes, such as by class, race, level, or specific characters
+        # use methods such .query(), .drop(), .isin(lists), or straight boolean to change the dataframe before passing it on to the other functions
+
+    # define method roster_statistics():
+        # find mean, median, max, and min attributes for all characters in roster
+        # maybe find amounts of each class or race as well
+
+    # define method compare_characters(char1,char2):
+        # similar to matplotlib comparison, maybe generate charts to compare attributes, then also compare race, class, and level
+
+    # define method creation_trends():
+        # see how many of each class there is in the roster, as well as race
